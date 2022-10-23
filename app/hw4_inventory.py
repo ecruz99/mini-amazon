7545a2f37a0002d3b4e4c621bfb7abcd2e57d2c3
Erik Cruz <erc37@duke.edu>
2022-10-23 18:41:23 -0400

    2nd commit by E.C

---
+++ app/hw4_inventory.py
@@ -0,0 +1,33 @@
+from flask import render_template
+from flask_login import current_user
+import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+
+from .models.product import Product
+from .models.purchase import Purchase
+
+from flask import Blueprint
+bp = Blueprint('hw4_inventory', __name__)
+
+class InventoryForm(FlaskForm):
+    sid_input = StringField('Seller ID')
+    submit = SubmitField('Submit')
+
+@bp.route('/sidinventory', methods = ["GET", "POST"])
+def sidinventory():
+    form = InventoryForm()
+    if form.validate_on_submit():
+        seller_inventory = Inventory.get_all_by_sid(form.sid_input.data)
+        return render_template('hw4_inventory.html',
+                           sid_inventory=seller_inventory,
+                           form=form
+                           )
+
+    # render the page by adding information to the index.html file
+    seller_inventory = Inventory.get_all_by_sid(form.sid_input.data)
+    return render_template('hw4_inventory.html',
+                           sid_inventory=seller_inventory,
+                           form=form
+                           )
