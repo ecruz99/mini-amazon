fe104efa7c4ed9218c82206f16e40e0bbe5b4d71
Erik Cruz <erc37@duke.edu>
2022-10-23 19:22:02 -0400

    3rd commit EC

---
+++ app/models/inventory.py
@@ -0,0 +1,33 @@
+from flask import current_app as app
+
+
+class Inventory:
+    def __init__(self, sellerID, productID, productname, quantity):
+        self.sellerID = sellerID
+        self.productID = productID
+        self.productname = productname
+        self.quantity = quantity
+
+
+    @staticmethod
+    def get(sellerID):
+        rows = app.db.execute('''
+SELECT sellerID, productID, productname, quantity
+FROM Inventory
+WHERE sellerID = :sellerID
+''',
+                              sellerID=sellerID)
+        return Inventory(*(rows[0])) if rows is not None else None
+
+
+    @staticmethod
+    def get_all_by_sid(sellerID, productID):
+        rows = app.db.execute('''
+SELECT sellerID, productID, productname, quantity
+FROM Inventory
+WHERE sellerID = :sellerID 
+AND productID = productID
+''',
+                              sellerID = sellerID
+                              productID = productID)
+        return [Inventory(*row) for row in rows]
