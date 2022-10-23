642f275ada00a1fd1f8b01c27ab9d5683c67bdd9
Brian Davis <bd134@duke.edu>
2022-10-23 14:43:22 -0400

    deleted unnecessary validate function

--- app/hw4.py
+++ app/hw4.py
@@ -18,17 +18,11 @@ class UserForm(FlaskForm):
 @bp.route('/uidpurch', methods = ["GET", "POST"])
 def uidpurch():
     form = UserForm()
-    if form.validate_on_submit():
-        purchases_by_user = Purchase.get_all_by_uid(form.uid_input.data)
-        return render_template('hw4.html',
-                           uid_purchases=purchases_by_user,
-                           form=form
-                           )
+    purchases_by_user = Purchase.get_all_by_uid(form.uid_input.data)
+    return render_template('hw4.html',
+                        uid_purchases=purchases_by_user,
+                        form=form
+                        )
     # purchases_by_user = Purchase.get_all_by_uid(2)
 
     # render the page by adding information to the index.html file
-    purchases_by_user = Purchase.get_all_by_uid(form.uid_input.data)
-    return render_template('hw4.html',
-                           uid_purchases=purchases_by_user,
-                           form=form
-                           )
