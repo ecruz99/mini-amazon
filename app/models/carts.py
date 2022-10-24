2b45078c7c0601f6d2a30ca29c5da1a95d98b64b
acd42 <acd42@duke.edu>
2022-10-24 17:52:26 -0400

    Update app/models/carts.py

--- app/models/carts.py
+++ app/models/carts.py
@@ -1,1 +1,24 @@
+from flask import current_app as app
+
+
+class Care:
+    def __init__(self, uid, pid, sid, productname, quantity, unit_price):
+        self.uid = uid
+        self.pid = pid
+        self.sid = sid
+        self.productname = productname
+        self.quantity = quantity
+        self.unit_price = unit_price
+
+    
+    @staticmethod
+    def get_cart(uid):
+        rows = app.db.execute('''
+SELECT uid, pid, sid, productname, quantity, unit_price
+FROM Carts
+WHERE uid = :uid
+''',
+                              uid=uid)
+        return [Carts(*row) for row in rows]
+
 
