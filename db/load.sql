f1b48f18c01cee1a172138c6c6c9e4013d79d3db
acd42 <acd42@duke.edu>
2022-10-24 18:50:14 -0400

    Update db/load.sql

--- db/load.sql
+++ db/load.sql
@@ -20,5 +20,5 @@ SELECT pg_catalog.setval('public.purchases_id_seq',
 
 \COPY Carts FROM 'Carts.csv' WITH DELIMITER ',' NULL '' CSV
 SELECT pg_catalog.setval('public.carts_id_seq',
-                         (SELECT MAX(id)+1 FROM Carts),
+                         (SELECT MAX(uid)+1 FROM Carts),
                          false);
