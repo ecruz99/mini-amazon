d7232a31d34d1c258734d0fa47db764b4a1d2397
Erik Cruz <erc37@duke.edu>
2022-10-23 23:29:14 -0400

    created inv table

--- db/load.sql
+++ db/load.sql
@@ -15,6 +15,6 @@ SELECT pg_catalog.setval('public.purchases_id_seq',
                          false);
 
 \COPY Inventory FROM 'Inventory.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.purchases_id_seq',
-                         (SELECT MAX(id)+1 FROM Purchases),
+SELECT pg_catalog.setval('public.seller_id_seq',
+                         (SELECT MAX(id)+1 FROM Inventory),
                          false);
