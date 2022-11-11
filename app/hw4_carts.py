bfd8da9be29d707de4dfdfcd446f43e3f3c3a5df
acd42 <acd42@duke.edu>
2022-11-11 13:37:39 -0500

    Update app/templates/hw4_carts.html, app/models/carts.py, app/hw4_carts.py

--- app/hw4_carts.py
+++ app/hw4_carts.py
@@ -17,30 +17,49 @@ class DeleteFromCartForm(FlaskForm):
     pid_input2 = StringField('Product ID', validators=[DataRequired()])
     submit2 = SubmitField('Delete') 
 
+class ChangeItemQuantity(FlaskForm):
+    uid_input3 = StringField('User ID', validators=[DataRequired()])
+    sid_input3 = StringField('Seller ID', validators=[DataRequired()])
+    pid_input3 = StringField('Product ID', validators=[DataRequired()])
+    quantity3 = StringField('Quantity', validators=[DataRequired()])
+    submit3 = SubmitField('Submit')
 
 @bp.route('/uidcart', methods = ["GET", "POST"])
 def uidcart():
     form = CartsForm()
     form2 = DeleteFromCartForm()
+    form3 = ChangeItemQuantity()
 
     if form.submit.data and form.validate():
         user_cart = Cart.get_cart(form.uid_input.data)
         return render_template('hw4_carts.html',
                         cart=user_cart,
                         form=form,
-                        form2=form2
+                        form2=form2,
+                        form3=form3
                         )
     elif form2.submit2.data and form2.validate():
         Cart.delete_cart_item(form2.uid_input2.data, form2.pid_input2.data, form2.sid_input2.data)
 
         return render_template('hw4_carts.html',
                     form=form,
-                    form2=form2
+                    form2=form2,
+                    form3=form3
+                    )
+    elif form3.submit3.data and form3.validate():
+        Cart.update_quantity(form3.uid_input3.data, form3.pid_input3.data, 
+                             form3.sid_input3.data, form3.quantity3.data)
+        
+        return render_template('hw4_carts.html',
+                    form=form,
+                    form2=form2,
+                    form3=form3
                     )
     else:
         return render_template('hw4_carts.html',
                     form=form,
-                    form2=form2
+                    form2=form2,
+                    form3=form3
                     ) 
 
     
