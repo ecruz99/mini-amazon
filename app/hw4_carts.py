88f2d819662e2ada0d5393867d88159ab1ef93a6
acd42 <acd42@duke.edu>
2022-11-11 06:02:37 -0500

    Update app/hw4_carts.py

--- app/hw4_carts.py
+++ app/hw4_carts.py
@@ -30,7 +30,7 @@ def uidcart():
                         form=form,
                         form2=form2
                         )
-    elif form2.submit.data and form2.validate():
+    elif form2.submit2.data and form2.validate():
         Cart.delete_cart_item(form2.uid_input2.data, form2.sid_input2.data, form2.pid_input2.data)
 
         return render_template('hw4_carts.html',
