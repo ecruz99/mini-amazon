99f1a756c3b78511c79aeea14c009bac053047ee
Brian Davis <bd134@duke.edu>
2022-12-07 18:55:32 -0500

    optional stuff for possibly combining views

--- app/users.py
+++ app/users.py
@@ -249,8 +249,8 @@ def seller_view():
 @bp.route('/publicview', methods=['GET', 'POST'])
 def public_view():
     req = request.args
-    uid = req.get("uid")
-    sid = req.get("sid")
+    uid = req.get("uid") if req.get("uid") else None
+    sid = req.get("sid") if req.get("sid") else None
 
     # PRODUCT REVIEWS WRITTEN
     recentPReviews = PReview.getUserProductReviews(uid)
@@ -293,17 +293,19 @@ def public_view():
 
     user = User.get(uid)
     user_name = user.firstname + ' ' + user.lastname
-    user_email = user.email
-    user_address = user.address
 
-    seller = User.get(uid)
-    seller_name = seller.firstname + ' ' + seller.lastname
+    seller, seller_name, seller_email, seller_address = None, None, None, None
+    seller = User.get(sid)
+    if seller != None:
+        seller_name = seller.firstname + ' ' + seller.lastname
+        seller_email = user.email
+        seller_address = user.address
 
     # Haven't finished this yet
-    return render_template('publicview.html', title='Public View', recentReviews = recentReviews, averageReview = averageReview, numberOfReview = numberOfReview,
-                           numberOfReviewOne = numberOfReviewOne, numberOfReviewTwo = numberOfReviewTwo, numberOfReviewThree = numberOfReviewThree,
-                           numberOfReviewFour = numberOfReviewFour, numberOfReviewFive = numberOfReviewFive, fullname=fullname, sid = id,
-                           sForm = sForm)
+    return render_template('publicview.html', title='Public View', recentPReviews = recentPReviews, averagePReview = averagePReview, numberOfPReview = numberOfPReview,
+                           numberOfPReviewOne = numberOfPReviewOne, numberOfPReviewTwo = numberOfPReviewTwo, numberOfPReviewThree = numberOfPReviewThree,
+                           numberOfPReviewFour = numberOfPReviewFour, numberOfPReviewFive = numberOfPReviewFive, user_name=user_name, uid = uid, sid = sid,
+                           sForm = sForm, seller_name=seller_name, seller_email=seller_email, seller_address=seller_address)
     
 @bp.route('/updatereview', methods = ["GET", "POST"])
 def updatereview():
